8bf1159fcbf9b73abd380587fb90aed9c453fc1c
erikdreifeldt1 <erikdr@student.chalmers.se>
2024-09-24 16:36:04 +0200

    Done the tables and inserts
    Co-Authored-By: TeeDestroyer <111562945+TeeDestroyer@users.noreply.github.com>

--- main.sql
+++ main.sql
@@ -1,22 +1,9 @@
-/*
-In Neon, databases are stored on branches. By default, a project has one branch and one database.
-You can select the branch and database to use from the drop-down menus above.
-
-Try generating sample data and querying it by running the example statements below, or click
-New Query to clear the editor.
-*/
-CREATE TABLE IF NOT EXISTS playing_with_neon(id SERIAL PRIMARY KEY, name TEXT NOT NULL, value REAL);
-INSERT INTO playing_with_neon(name, value)
-  SELECT LEFT(md5(i::TEXT), 10), random() FROM generate_series(1, 10) s(i);
-SELECT * FROM playing_with_neon;
-
-CREATE TABLE Products 
-
 CREATE TABLE IF NOT EXISTS colour (
 product_id int primary key REFERENCES Products.id,
 colour_category varchar(50) not null,
 colour_name varchar(50) not null,
-price int not null);
+price int not null
+);
 
 CREATE TABLE IF NOT EXISTS Models (
 series CHAR NOT NULL,
@@ -25,7 +12,7 @@ price INT NOT NULL,
 width INT NOT NULL,
 depth INT NOT NULL,
 height INT NOT NULL,
-weight INT NOT NULL,
+weight NUMBER NOT NULL,
 PRIMARY KEY (series, model)
 );
 
